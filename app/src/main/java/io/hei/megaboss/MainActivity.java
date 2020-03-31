ce39788eae82c4d581ed2b77a849d445b4e33e3e
eliottlenud <eliott.le-nud@hei.yncrea.fr>
2020-03-31 13:01:23 +0200

    first

--- app/src/main/java/io/hei/megaboss/MainActivity.java
+++ app/src/main/java/io/hei/megaboss/MainActivity.java
@@ -11,9 +11,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.lettres);
+        setContentView(R.layout.activity_main);
 
-        Random rand = new Random();
-        char c = (char)(rand.nextInt(26) + 97);
+
     }
 }
